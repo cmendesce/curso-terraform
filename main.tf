1850ad3de86fccb1df063cc852445aaef8c99783
Carlos Mendes <cmendesce@gmail.com>
2021-01-20 21:14:26 -0300

    Add backend

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 0.11.0"
+  backend "s3" {
+    bucket = "uece-terraform-backend"
+    key    = "state.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
@@ -11,4 +16,18 @@ resource "aws_instance" "ubuntu" {
   instance_type = var.instance_type
   availability_zone = var.az
   monitoring = true
+  count = 2
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "um-bucket-privado-uece"
+  acl    = "private"
+
+  tags = {
+    Environment = "Dev"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public_dns" {
-  value = aws_instance.ubuntu.public_dns
+  value = aws_instance.ubuntu[1].public_dns
 }
 
 output "instance_id" {
-  value = aws_instance.ubuntu.id
+  value = aws_instance.ubuntu[1].id
 }
